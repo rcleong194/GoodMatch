4413eb1cf1844aa391c5af95cd7d8bf8479dd78d
u14277469 <43065784+u14277469@users.noreply.github.com>
2021-09-15 21:53:42 +0200

    Second part
    Added a few .csv files for output.
    Added csv functionality
    Allow the user to choose whether they want to use the console or input a csv file

--- app.js
+++ app.js
@@ -1,16 +1,84 @@
 'use strict';
 var readlineSync = require('readline-sync');
+var parser = require('csv-parse/lib/sync');
 
-let name1 = readlineSync.question("Input name 1: ");
-numCheck(name1);
-let name2 = readlineSync.question("Input name 2: ");
-numCheck(name2);
+var fs = require('fs');
 
-console.log(getCompatabilityOfPlayers(name1, name2));
+let conOrcsv = readlineSync.question("Would you like to use the console or input .csv file? (con/csv)");
+if (conOrcsv == 'con') {
+    let name1 = readlineSync.question("Input name 1: ");
+    numCheck(name1);
+    let name2 = readlineSync.question("Input name 2: ");
+    numCheck(name2);
 
-function getCompatabilityOfPlayers(n1,n2) {
+    console.log(printCompatabilityOfPlayers(name1, name2));
+} else if (conOrcsv == 'csv') {
+    try {
+        let fileName = readlineSync.question("Full file name (E.g. foo.csv)");
+        const fileData = fs.readFileSync('./' + fileName, { encoding: 'utf8', flag: 'r' });
+
+        const records = parser(fileData, {
+            rows: true,
+            skip_empty_lines: true
+        })
+
+        pairMaleFemale(records);
+    } catch {
+        console.log("File does not exist");
+        process.exit();
+    }
     
 
+} else {
+    console.log("Please only put 'con' or 'csv'");
+    process.exit();
+}
+
+
+
+function pairMaleFemale(data) {
+    let males = [];
+    let females = [];
+    let pairResults = [];
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].length > 2) {
+            console.log("Only input the name and gender separated by a comma (E.g. John, m)");
+            process.exit();
+        }
+        numCheck(data[i][0]);
+        let gender = data[i][1].replace(/\s+/g, '');
+        switch (gender) {
+            case 'f':
+                females.push(data[i][0]);
+                break;
+            case 'm':
+                males.push(data[i][0]);
+                break;
+            default:
+                console.log("Gender must be 'f' or 'm'");
+                process.exit();
+                break;
+        }
+    }
+    males = removeDuplicatesAndSort(males);
+    females = removeDuplicatesAndSort(females);
+    for (let i = 0; i < males.length; i++) {
+        for (let j = 0; j < females.length; j++) {
+            let compat = getCompatabilityOfPlayers(males[i], females[j]);
+            pairResults.push([compat,males[i], females[j]])
+        }
+    }
+    pairResults = pairResults.sort(function (a, b) { return b[0] - a[0]; });
+    let txt = toTextFile(pairResults);
+    var file = fs.createWriteStream('output.txt');
+    file.on('error', function (err) {  });
+    txt.forEach(function (v) { file.write(v + '\n'); });
+    file.end();
+    //return [males, females];
+}
+
+///Older code for console use.
+ function printCompatabilityOfPlayers(n1, n2) {
     let finalString = n1 + "matches" + n2;
 
     finalString = finalString.toLowerCase();
@@ -23,6 +91,21 @@ function getCompatabilityOfPlayers(n1,n2) {
     let retVal = parseFloat(compatString) > 80 ? n1 + ' matches ' + n2 + ' ' + compatString + '%, good match' : n1 + ' matches ' + n2 + ' ' + compatString + '%';
     return retVal;
 }
+
+function getCompatabilityOfPlayers(n1, n2) {
+    let finalString = n1 + "matches" + n2;
+
+    finalString = finalString.toLowerCase();
+    finalString = finalString.replace(/\s+/g, '');
+    let compatArray = getCompatablity(characterCounter(finalString));
+    let compatString = '';
+    for (let i = 0; i < compatArray.length; i++) {
+        compatString += compatArray[i];
+    }
+    let retVal = parseFloat(compatString);
+    return retVal;
+}
+
 
 function characterCounter(s) {
     
@@ -44,8 +127,8 @@ function characterCounter(s) {
 
 function numCheck(s) {
     for (let i = 0; i < s.length; i++) {
-        if (s[i].match(/[A-Z|a-z]/i)==null) {
-            console.log("Please only input letters");
+        if (s[i].match(/[A-Z|a-z]/i) == null) {
+            console.log("Names can only be letters");
             process.exit();
         }
     }
@@ -53,7 +136,6 @@ function numCheck(s) {
 
 function getCompatablity(numArray) {
     let workingArray = [];
-    //console.log(numArray);
     numArray = splitNumArray(numArray);
     if (numArray.length <= 2)
         return numArray;
@@ -63,7 +145,6 @@ function getCompatablity(numArray) {
         let num2 = numArray.pop() || 0;
         workingArray.push(num1 + num2);
     }
-    console.log(workingArray);
     return getCompatablity(workingArray);
 }
 
@@ -76,4 +157,14 @@ function splitNumArray(numArray) {
     return splitArr.map(x => parseFloat(x));
 }
 
-//console.log(percentage(name1));
+function removeDuplicatesAndSort(arr) {
+    return [...new Set(arr)].sort();
+}
+
+
+function toTextFile(arr) {
+    let retArray =[]
+    for (let i = 0; i < arr.length; i++)
+        retArray.push(arr[i][0] > 80 ? arr[i][1] + ' matches ' + arr[i][2] + ' ' + arr[i][0] + '%, good match' : arr[i][1] + ' matches ' + arr[i][2] + ' ' + arr[i][0] + '%');
+    return retArray;
+}
